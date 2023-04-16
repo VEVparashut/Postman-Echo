53453cffa71608976ce143a7f18ee9664645bd95
Neto test <89373786148@yandex.ru>
2023-04-16 16:47:37 +0300

    inicial commit

---
+++ src/test/java/postmanEchoTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+public class postmanEchoTest {
+    @Test
+    void postmanTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hello World")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello World"));
+    }
+}
